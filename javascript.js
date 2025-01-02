2b9e7165a7eac5aa9f7c99c7a49497d332ffdb03
Yue <190688943+yuexiaohuaz@users.noreply.github.com>
2025-01-01 20:22:09 -0600

    Completed project.

--- javascript.js
+++ javascript.js
@@ -11,9 +11,32 @@ popup for the number of squares per side.
 */
 
 const container = document.querySelector("#container");
-container.style.height = "600px";
-container.style.width = "600px";
+const body = document.querySelector("body");
+const button = document.createElement("button");
 
+container.style.height = "650px";
+container.style.width = "650px";
+
+button.textContent = "Click me to regenerate grid!";
+button.addEventListener("click", () => {
+    let size = prompt("How large will the new grid be?");
+    while (size > 100 || size < 0 || isNaN(size)) {
+        size = prompt("That is an invalid size! Enter a different value");
+    }
+    deleteGrid();
+    generateGrid(size);
+})
+body.insertBefore(button, container);
+
+function getRandom() {
+    return Math.floor(Math.random() * 256);
+}
+function deleteGrid() {
+    rows = document.querySelectorAll(".row");
+    for (row of rows) {
+        container.removeChild(row);
+    }
+}
  //make as many subdivs representing for rows 
 function generateGrid(size = 16) {
     //generates divs and adds a class to it 
@@ -26,10 +49,15 @@ function generateGrid(size = 16) {
         for (let j = 0; j < size; j++) {
             const box = document.createElement("div");
             box.classList.add("box");
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = `rgb(${getRandom()}, ${getRandom()}, ${getRandom()})`;
+                // box.style.backgroundColor = "rgb(255, 120, 120)";
+            });
             row.appendChild(box);
 
         }
         
     }
 }
+
 generateGrid(12);
